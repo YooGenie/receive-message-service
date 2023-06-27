df9d5bb63a46cd8fd3a3342e825cacabcf7caa87
sunny <yshee712@gmail.com>
2023-06-27 11:30:31 +0900

    #21 서비스가 죽지 않고 계속 돌게만들기

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"receive-message-service/service"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 )
@@ -16,25 +17,29 @@ func main() {
 	timeout := int64(5)
 	queueURL := ""
 
-	msgResult, err := service.GetMessages(sess, &queueURL, &timeout)
-	if err != nil {
-		log.Println("Got an error receiving messages:")
-	}
+	for {
+		time.Sleep(time.Second * 5)
 
-	if len(msgResult.Messages) != 0 {
-		for i, _ := range msgResult.Messages {
-			fmt.Println(i, "번째 메시지 바디내용", *msgResult.Messages[i].Body)
-			messageHandle := *msgResult.Messages[i].ReceiptHandle
-			if messageHandle == "" {
-				log.Println("You must supply message receipt handle (-m MESSAGE-HANDLE)")
-			}
-			err = service.DeleteMessage(sess, &queueURL, &messageHandle)
-			if err != nil {
-				log.Println("Got an error deleting the message:")
-			}
-			log.Println("Deleted message from queue with URL " + queueURL)
+		msgResult, err := service.GetMessages(sess, &queueURL, &timeout)
+		if err != nil {
+			log.Println("Got an error receiving messages:")
 		}
-	} else {
-		log.Println("서비스2 -큐에 아무것도 없음!!")
+
+		if len(msgResult.Messages) != 0 {
+			for i, _ := range msgResult.Messages {
+				fmt.Println(i, "번째 메시지 바디내용", *msgResult.Messages[i].Body)
+				messageHandle := *msgResult.Messages[i].ReceiptHandle
+				if messageHandle == "" {
+					log.Println("You must supply message receipt handle (-m MESSAGE-HANDLE)")
+				}
+				err = service.DeleteMessage(sess, &queueURL, &messageHandle)
+				if err != nil {
+					log.Println("Got an error deleting the message:", err)
+				}
+				log.Println("Deleted message from queue with URL " + queueURL)
+			}
+		} else {
+			log.Println("서비스2 -큐에 아무것도 없음!!")
+		}
 	}
 }
